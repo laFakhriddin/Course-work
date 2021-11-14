097b60457a73671267a230de7b74405e5182695a
Fakhriddin <tanto.fn@gmail.com>
2021-11-15 02:56:31 +0500

    night by night work course work v1.5

--- src/course/work/intro.java
+++ src/course/work/intro.java
@@ -101,7 +101,7 @@ public class intro extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        design ds = new design();
+        graphic ds = new graphic();
         ds.show();
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
